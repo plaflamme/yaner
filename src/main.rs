44ab9cd5b81e11a944e02714db8e62f92638106f
Philippe Laflamme <philippe.laflamme@gmail.com>
2019-11-19 22:23:06 -0500

    Switch to structopt

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,36 @@
-use clap::{Arg, App, SubCommand};
 use std::convert::TryFrom;
 use std::fs::File;
 use std::io::Read;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 mod memory;
 mod cpu;
 mod nes;
 mod rom;
 
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Yet Another NES Emulator in Rust")]
+enum Yaner {
+    Generate,
+    Info {
+        rom: PathBuf
+    }
+}
+
 fn main() {
-
-    let matches = App::new("rnes")
-        .subcommand(
-            SubCommand::with_name("generate").arg(
-                Arg::with_name("opcode").short("o").long("opcodes").help("Generate OpCode table")
-            )
-        )
-        .subcommand(
-            SubCommand::with_name("info").arg(
-                Arg::with_name("rom").short("r").long("rom").required(true).takes_value(true).help("ROM file")
-            )
-        )
-        .get_matches();
-
-    match matches.subcommand() {
-        ("generate", Some(args)) => {
-            if args.is_present("opcode") {
-                cpu::generator::generate_opcode_table()
-            }
+    let options = Yaner::from_args();
+    use Yaner::*;
+    match options {
+        Generate => {
+            cpu::generator::generate_opcode_table()
         },
-        ("info", Some(args)) => {
-            match args.value_of("rom") {
-                Some(filename) => {
-                    let mut f = File::open(filename).unwrap();
-                    let mut buffer = Vec::new();
-                    // read the whole file
-                    f.read_to_end(&mut buffer).unwrap();
-                    println!("{:?}", rom::Rom::try_from(buffer.as_slice()).unwrap());
-                },
-                None => unimplemented!()
-            }
-        },
-        _ => unimplemented!()
+        Info { rom } => {
+            let mut f = File::open(rom).unwrap();
+            let mut buffer = Vec::new();
+            // read the whole file
+            f.read_to_end(&mut buffer).unwrap();
+            println!("{:?}", rom::Rom::try_from(buffer.as_slice()).unwrap());
+        }
     }
 }
