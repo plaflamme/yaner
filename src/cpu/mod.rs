754d7f2eaa0f0919ebaeb58a49531428ae7bad1b
plaflamme <philippe@hopper.com>
2019-11-24 20:00:28 -0500

    PHA/P and PLA/P stack operations.

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -301,6 +301,19 @@ impl Cpu {
                         yield_complete!(stack::jsr(self, mem_map));
                     },
 
+                    OpCode(Op::PHA, AddressingMode::Implicit) => {
+                        yield_complete!(stack::pha(self, mem_map));
+                    },
+                    OpCode(Op::PHP, AddressingMode::Implicit) => {
+                        yield_complete!(stack::php(self, mem_map));
+                    },
+                    OpCode(Op::PLA, AddressingMode::Implicit) => {
+                        yield_complete!(stack::pla(self, mem_map));
+                    },
+                    OpCode(Op::PLP, AddressingMode::Implicit) => {
+                        yield_complete!(stack::plp(self, mem_map));
+                    },
+
                     OpCode(Op::RTS, AddressingMode::Implicit) => {
                         yield_complete!(stack::rts(self, mem_map));
                     },
@@ -925,6 +938,7 @@ impl ImplicitOperation for txs {
 
 mod stack {
     use super::*;
+
     //  #  address R/W description
     // --- ------- --- -------------------------------------------------
     //  1    PC     R  fetch opcode, increment PC
@@ -934,7 +948,7 @@ mod stack {
     //  5  $0100,S  W  push PCL on stack, decrement S
     //  6    PC     R  copy low address byte to PCL, fetch high address
     //                 byte to PCH
-    pub(super) fn jsr<'a>(cpu: &'a Cpu, mem_map: &'a Box<&dyn AddressSpace>) -> impl Generator<Yield = CpuCycle, Return = ()> + 'a {
+    pub(super) fn jsr<'a>(cpu: &'a Cpu, mem_map: &'a Box<&dyn AddressSpace>) -> impl Generator<Yield=CpuCycle, Return=()> + 'a {
         move || {
             let addr_lo = cpu.pc_read_u8_next(mem_map) as u16;
             yield CpuCycle::Tick;
@@ -980,6 +994,61 @@ mod stack {
 
             let pc = ((pc_hi << 8) | pc_lo).wrapping_add(1);
             cpu.pc.set(pc);
+            yield CpuCycle::Tick;
+        }
+    }
+
+    //  #  address R/W description
+    // --- ------- --- -----------------------------------------------
+    //  1    PC     R  fetch opcode, increment PC
+    //  2    PC     R  read next instruction byte (and throw it away)
+    //  3  $0100,S  W  push register on stack, decrement S
+    fn push<'a>(cpu: &'a Cpu, mem_map: &'a Box<&dyn AddressSpace>, value: u8) -> impl Generator<Yield=CpuCycle, Return=()> + 'a {
+        move || {
+            let _ = cpu.pc_read_u8_next(mem_map);
+            yield CpuCycle::Tick;
+
+            cpu.push_stack(mem_map, value);
+            yield CpuCycle::Tick;
+        }
+    }
+    pub(super) fn php<'a>(cpu: &'a Cpu, mem_map: &'a Box<&dyn AddressSpace>) -> impl Generator<Yield=CpuCycle, Return=()> + 'a {
+        push(cpu, mem_map, cpu.flags.get().bits())
+    }
+    pub(super) fn pha<'a>(cpu: &'a Cpu, mem_map: &'a Box<&dyn AddressSpace>) -> impl Generator<Yield=CpuCycle, Return=()> + 'a {
+        push(cpu, mem_map, cpu.acc.get())
+    }
+
+    //  #  address R/W description
+    // --- ------- --- -----------------------------------------------
+    //  1    PC     R  fetch opcode, increment PC
+    //  2    PC     R  read next instruction byte (and throw it away)
+    //  3  $0100,S  R  increment S
+    //  4  $0100,S  R  pull register from stack
+    fn pull<'a>(cpu: &'a Cpu, mem_map: &'a Box<&dyn AddressSpace>) -> impl Generator<Yield=CpuCycle, Return=u8> + 'a {
+        move || {
+            let _ = cpu.pc_read_u8_next(mem_map);
+            yield CpuCycle::Tick;
+
+            cpu.stack_dec();
+            yield CpuCycle::Tick;
+
+            cpu.read_stack(mem_map)
+        }
+    }
+
+    pub(super) fn plp<'a>(cpu: &'a Cpu, mem_map: &'a Box<&dyn AddressSpace>) -> impl Generator<Yield=CpuCycle, Return=()> + 'a {
+        move || {
+            let value = yield_complete!(pull(cpu, mem_map));
+            cpu.flags.set(Flags::from_bits_truncate(value));
+            yield CpuCycle::Tick;
+        }
+    }
+
+    pub(super) fn pla<'a>(cpu: &'a Cpu, mem_map: &'a Box<&dyn AddressSpace>) -> impl Generator<Yield=CpuCycle, Return=()> + 'a {
+        move || {
+            let value = yield_complete!(pull(cpu, mem_map));
+            cpu.acc.set(value);
             yield CpuCycle::Tick;
         }
     }
