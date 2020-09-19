8f5c0de63ff190a97d2133e6dc438e725c66d9c3
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-09-19 17:14:09 -0400

    Adds CPU timing test.
    I'm suspecting that the issues I have with sprite-0 hits are related to
    CPU-PPU timings because the test uses a delay to turn on/off the PPU
    which seems to occur at the wrong time...

--- tests/cpu.rs
+++ tests/cpu.rs
@@ -2,8 +2,10 @@
 
 mod common;
 
+use common::run_test;
 use common::blargg::run_blargg_test;
 use test_case::test_case;
+use std::path::Path;
 
 #[test_case("01-basics")]
 #[test_case("02-implied")]
@@ -31,4 +33,33 @@ fn instr_test(case: &str) {
 #[test_case("04-dummy_reads_apu" => panics "not implemented")] // TODO
 fn instr_misc(case: &str) {
     run_blargg_test(format!("roms/nes-test-roms/instr_misc/rom_singles/{}.nes", case).as_str());
+}
+
+#[test]
+fn cpu_timing_test() {
+    let mut frames = 0;
+    let nes = run_test(&Path::new("roms/nes-test-roms/cpu_timing_test6/cpu_timing_test.nes"), None, |_| {
+        frames += 1;
+        frames >= 640
+    });
+
+    // NOTE: these were shown to be different when the test fails. It's not documented anywhere though...
+    assert_eq!(nes.debug().cpu_bus.read_u8(0x0000), 0x60);
+    assert_eq!(nes.debug().cpu_bus.read_u8(0x0001), 0xE6);
+}
+
+
+#[test_case("1.Branch_Basics")]
+#[test_case("2.Backward_Branch")]
+#[test_case("3.Forward_Branch")]
+fn cpu_branch_timing_test(case: &str) {
+    let mut frames = 0;
+    let nes = run_test(&Path::new(format!("roms/nes-test-roms/branch_timing_tests/{}.nes", case).as_str()), None, |_| {
+        frames += 1;
+        frames >= 30
+    });
+
+    // NOTE: these were shown to be different when the test fails. It's not documented anywhere though...
+    assert_eq!(nes.debug().cpu_bus.read_u8(0x0000), 0x80);
+    assert_eq!(nes.debug().cpu_bus.read_u8(0x0001), 0xE0);
 }
