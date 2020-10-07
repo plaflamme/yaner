59927c5487a05db682bb79669fe8bbeffebb39cb
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-10-06 20:24:25 -0400

    Adds tests for joypad.

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -60,3 +60,81 @@ impl Input for Joypad {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_joypad_strobe_high() {
+        // when strobe is high, reading should always return the first bit (button A)
+        let joypad = Joypad::default();
+        joypad.update(JoypadButtons::A);
+        joypad.strobe(1);
+        assert_eq!(1, joypad.read());
+        assert_eq!(1, joypad.read());
+        joypad.update(JoypadButtons::empty());
+        assert_eq!(0, joypad.read());
+        assert_eq!(0, joypad.read());
+    }
+
+    #[test]
+    fn test_joypad_strobe_low() {
+        // when strobe is high, reading should return values in the following order:
+        // A, B, Select, Start, Up, Down, Left, Right
+        let order = vec![
+            JoypadButtons::A,
+            JoypadButtons::B,
+            JoypadButtons::Select,
+            JoypadButtons::Start,
+            JoypadButtons::Up,
+            JoypadButtons::Down,
+            JoypadButtons::Left,
+            JoypadButtons::Right,
+        ];
+        for btn in order {
+            let joypad = Joypad::default();
+            joypad.strobe(1);
+            joypad.update(btn);
+            joypad.strobe(0);
+            let mut bits = 0u8;
+            for i in 0..8 {
+                let value = joypad.read();
+                bits |= value << i;
+            }
+            assert_eq!(btn, JoypadButtons::from_bits(bits).unwrap());
+        }
+    }
+
+    #[test]
+    fn test_joypad_read_multiple() {
+        let multiple = JoypadButtons::A | JoypadButtons::Select | JoypadButtons::Up | JoypadButtons::Left;
+        let joypad = Joypad::default();
+        joypad.strobe(1);
+        joypad.update(multiple);
+        joypad.strobe(0);
+        let mut bits = 0u8;
+        for i in 0..8 {
+            let value = joypad.read();
+            bits |= value << i;
+        }
+        assert_eq!(multiple, JoypadButtons::from_bits(bits).unwrap());
+    }
+
+    #[test]
+    fn test_joypad_read_end() {
+        let joypad = Joypad::default();
+        joypad.strobe(1);
+        joypad.update(JoypadButtons::empty());
+        joypad.strobe(0);
+        let mut bits = 0u8;
+        for i in 0..8 {
+            let value = joypad.read();
+            bits |= value << i;
+        }
+        assert_eq!(0, bits);
+        // reading from this point should always return 1
+        assert_eq!(1, joypad.read());
+        assert_eq!(1, joypad.read());
+    }
+}
