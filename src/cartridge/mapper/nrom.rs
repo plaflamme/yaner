b7dfaa77b6ba088685cb89797e5d3ee4615251bf
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-10-07 21:53:47 -0400

    Update nrom to use bank switching.

--- src/cartridge/mapper/nrom.rs
+++ src/cartridge/mapper/nrom.rs
@@ -1,19 +1,27 @@
 use super::{Mapper, NametableMirroring};
-use crate::cartridge::rom::Rom;
+use crate::cartridge::rom::{Rom, Chr};
 use crate::cartridge::rom::RomData;
-use crate::memory::AddressSpace;
+use crate::memory::{AddressSpace, Ram};
+use crate::cartridge::mapper::{Switched, BankSelect};
 
 // http://wiki.nesdev.com/w/index.php/NROM
 pub struct NROM {
     mirroring: NametableMirroring,
-    data: RomData,
+    prg_ram: Ram,
+    prg_rom: Switched<crate::memory::Rom>,
+    chr: Chr,
 }
 
 impl From<&Rom> for NROM {
     fn from(rom: &Rom) -> Self {
+        let data = RomData::new(&rom);
+        let prg_rom =
+            Switched::new(crate::memory::Rom::new(data.prg_rom.clone()), data.prg_rom.len(), 16_384);
         NROM {
             mirroring: rom.nametable_mirroring,
-            data: RomData::new(&rom),
+            prg_ram: data.prg_ram,
+            prg_rom,
+            chr: data.chr, // wiki says there's no chr_ram, but it seems like some tests assume this is supported anyway
         }
     }
 }
@@ -37,22 +45,21 @@ impl Mapper for NROM {
 impl AddressSpace for NROM {
     fn read_u8(&self, addr: u16) -> u8 {
         match addr {
-            0x0000..=0x1FFF => self.data.chr.read_u8(addr),
+            0x0000..=0x1FFF => self.chr.read_u8(addr),
             // PRG RAM,
-            0x6000..=0x7FFF => self.data.prg_ram.read_u8(addr - 0x6000),
+            0x6000..=0x7FFF => self.prg_ram.read_u8(addr - 0x6000),
             // 0x8000..=0xBFFF - First 16 KB of ROM.
+            0x8000..=0xBFFF => self.prg_rom.read_u8(BankSelect::First, addr - 0x8000),
             // 0xC000..=0xFFFF - Last 16 KB of ROM (NROM-256) or mirror of $8000-$BFFF (NROM-128).
-            0x8000..=0xFFFF => {
-                self.data.prg_rom[(addr - 0x8000) as usize % self.data.prg_rom.len()]
-            }
+            0xC000..=0xFFFF => self.prg_rom.read_u8(BankSelect::Last, addr - 0xC000),
             _ => invalid_address!(addr),
         }
     }
 
     fn write_u8(&self, addr: u16, value: u8) {
         match addr {
-            0x0000..=0x1FFF => self.data.chr.write_u8(addr, value),
-            0x6000..=0x7FFF => self.data.prg_ram.write_u8(addr - 0x6000, value),
+            0x0000..=0x1FFF => self.chr.write_u8(addr, value),
+            0x6000..=0x7FFF => self.prg_ram.write_u8(addr - 0x6000, value),
             0x8000..=0xFFFF => panic!("tried writing to a read only location 0x{:X?}", addr),
             _ => invalid_address!(addr),
         }
