58d5f674020ffb128a3e3ec2ef372ecbd8032c36
Philippe Laflamme <philippe.laflamme@gmail.com>
2024-02-06 23:55:36 -0500

    feat: implement frame counter

--- src/apu/mod.rs
+++ src/apu/mod.rs
@@ -1,11 +1,13 @@
-use std::ops::Coroutine;
+use std::{ops::Coroutine, rc::Rc};
 
 mod frame_counter;
 
 use frame_counter::FrameCounter;
 
+use crate::memory::AddressSpace;
+
 pub enum ApuCycle {
-    Tick,
+    Tick { irq: bool },
 }
 
 pub struct Apu {
@@ -18,11 +20,30 @@ impl Apu {
             frame_counter: FrameCounter::new(),
         }
     }
+
     pub fn run(&self) -> impl Coroutine<Yield = ApuCycle, Return = ()> + '_ {
         #[coroutine]
         move || loop {
-            self.frame_counter.tick();
-            yield ApuCycle::Tick
+            if let Some(clock) = self.frame_counter.tick() {
+                yield ApuCycle::Tick {
+                    irq: clock.raise_interrupt,
+                }
+            } else {
+                yield ApuCycle::Tick { irq: false }
+            }
+        }
+    }
+}
+
+impl AddressSpace for Apu {
+    fn read_u8(&self, addr: u16) -> u8 {
+        todo!()
+    }
+
+    fn write_u8(&self, addr: u16, value: u8) {
+        match addr {
+            0x4017 => self.frame_counter.write(value),
+            _ => (),
         }
     }
 }
